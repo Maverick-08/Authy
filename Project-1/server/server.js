bbe0f95c9c570840e587d05ee2154d20718bfdd2
Vivek <ojhavivek24@gmail.com>
2024-06-26 09:25:29 +0530

    initial server configs

---
+++ Project-1/server/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import {config} from 'dotenv';
+
+const app = express();
+
+config();
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("Working");
+})
+
+app.listen(process.env.PORT,()=>{console.log("Server is running at port 3000")})
