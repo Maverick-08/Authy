ce651a4538a5db976e379bf0130153496d7a6db8
Vivek <ojhavivek24@gmail.com>
2024-07-15 17:54:54 +0530

    added admin notifications

--- Project-1/server/controllers/accessController.js
+++ Project-1/server/controllers/accessController.js
@@ -3,91 +3,103 @@ import { Users } from "../models/newUser.js";
 import { Notifications } from "../models/notifications.js";
 
 
-export const accessLevelHandler = async (req,res) => {
-    try{
+export const accessLevelHandler = async (req, res) => {
+    try {
         const payload = req.body; // {userId:"",requestingAccess:"Editor/Admin"};
-        const user = Users.findOne({id: payload.userId})
-        
-        if(!user){
-            return res.status(statusCodes.badRequest).json({msg:"Invalid user id"})
-        }
-        
-        await Users.updateOne({id: payload.userId},{requestingAccess:payload.requestingAccess})
+        const user = await Users.findOne({ id: payload.userId })
 
-        const userNotification = await Notifications.findOne({id: payload.userId})
-
-        if(!userNotification){
-            await Notifications.create({id: payload.userId, notifications:[`Request for ${payload.requestingAccess} access has been sent`]})
+        if (!user) {
+            return res.status(statusCodes.badRequest).json({ msg: "Invalid user id" })
         }
-        else{
-            if(userNotification.notifications.length >= 5){
+
+        await Users.updateOne({ id: payload.userId }, { requestingAccess: payload.requestingAccess })
+
+        const userNotification = await Notifications.findOne({ id: payload.userId })
+
+        if (!userNotification) {
+            await Notifications.create({ id: payload.userId, notifications: [`Request for ${payload.requestingAccess} access has been sent`] })
+        }
+        else {
+            if (userNotification.notifications.length >= 5) {
                 let existingNotifications = userNotification.notifications;
                 existingNotifications.pop();
                 existingNotifications.unshift(`Request for ${payload.requestingAccess} access has been sent`)
 
-                await Notifications.updateOne({id: payload.userId,notifications: existingNotifications})
+                await Notifications.updateOne({ id: payload.userId, notifications: existingNotifications })
             }
-            else{
+            else {
                 let existingNotifications = userNotification.notifications;
                 existingNotifications.unshift(`Request for ${payload.requestingAccess} access has been sent`)
-                await Notifications.updateOne({id: payload.userId,notifications: existingNotifications})
+                await Notifications.updateOne({ id: payload.userId, notifications: existingNotifications })
             }
+        }
+
+        const adminNotifications = await Notifications.findOne({ id: "2XNYDwq19L" })
+
+        if (!adminNotifications) {
+            await Notifications.create({ id: "2XNYDwq19L", notifications: [`${user.username} has requested ${payload.requestingAccess} access`] })
+        }
+        else {
+            let newNotifications = adminNotifications.notifications
+            newNotifications.push(`${user.username} has requested ${payload.requestingAccess} access`)
+            
+            await Notifications.updateOne({ id: "2XNYDwq19L" }, { notifications: newNotifications })
         }
 
         return res.sendStatus(statusCodes.noContent);
     }
-    catch(err){
-        console.log("@accessLevelhandler : \n"+err);
+    catch (err) {
+        console.log("@accessLevelhandler : \n" + err);
         return res.status(statusCodes.internalServerError)
     }
 
 }
 
-export const grantAccessHandler = async (req,res) => {
-    try{
+export const grantAccessHandler = async (req, res) => {
+    try {
         const payload = req.body; // {userId: "",requestStatus: true/false,grantRole: ""}
         console.log(payload);
-        const user = Users.findOne({id: payload.userId});
+        const user = Users.findOne({ id: payload.userId });
 
-        if(!user){
-            return res.status(statusCodes.badRequest).json({msg:"Invalid user id"})
+        if (!user) {
+            return res.status(statusCodes.badRequest).json({ msg: "Invalid user id" })
         }
 
-        if(payload.requestStatus){
-            await Users.updateOne({id: payload.userId},{role: payload.grantRole, requestingAccess: null})
+        if (payload.requestStatus) {
+            await Users.updateOne({ id: payload.userId }, { role: payload.grantRole, requestingAccess: null })
 
-            let existingNotifications = (await Notifications.findOne({id: payload.userId})).notifications;
+            let existingNotifications = (await Notifications.findOne({ id: payload.userId })).notifications;
 
             existingNotifications.unshift(`${payload.grantRole} access has been granted`)
 
-            await Notifications.updateOne({id: payload.userId},{notifications: existingNotifications})
+            await Notifications.updateOne({ id: payload.userId }, { notifications: existingNotifications })
         }
-        else{
-            await Users.updateOne({id: payload.userId},{requestingAccess: null})
+        else {
+            await Users.updateOne({ id: payload.userId }, { requestingAccess: null })
 
-            let existingNotifications = (await Notifications.findOne({id: payload.userId})).notifications;
+            let existingNotifications = (await Notifications.findOne({ id: payload.userId })).notifications;
 
             existingNotifications.unshift(`Request for ${payload.grantRole} access has been declined`)
 
-            await Notifications.updateOne({id: payload.userId},{notifications: existingNotifications})
+            await Notifications.updateOne({ id: payload.userId }, { notifications: existingNotifications })
         }
 
         return res.sendStatus(statusCodes.noContent)
     }
-    catch(err){
-        console.log("@grantAccessHandler : \n"+err);
-        return res.status(statusCodes.internalServerError).json({msg: err.name})
+    catch (err) {
+        console.log("@grantAccessHandler : \n" + err);
+        return res.status(statusCodes.internalServerError).json({ msg: err.name })
     }
 }
 
 export const userAccessList = async (req, res) => {
-    try{
+    try {
         const usersList = await Users.find({ requestingAccess: { $ne: null } });
 
-        return res.json({users:usersList})
+        return res.json({ users: usersList })
     }
-    catch(err){
-        console.log("@userAccessList : \n"+err);
+    catch (err) {
+        console.log("@userAccessList : \n" + err);
         return res.status(statusCodes.internalServerError)
     }
 }
