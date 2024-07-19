4871d5936dd66b1cb842966fe5ad6fb8ccb105e1
Vivek <ojhavivek24@gmail.com>
2024-07-19 22:09:56 +0530

    Implemented display notifications functionality

--- Project-2/client/src/components/Navbar.jsx
+++ Project-2/client/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { Link } from "react-scroll";
 import { useRecoilValue } from "recoil";
-import { userAtom } from "../state/userState";
+import { accessTokenAtom, userAtom } from "../state/userState";
 import NotificationIcon from "../assets/icons/notifications.svg";
+import axios from "axios";
 
 const Navbar = () => {
   const user = useRecoilValue(userAtom);
@@ -19,9 +20,16 @@ const Navbar = () => {
 };
 
 const User = ({ user }) => {
+  const [showNotification, setShowNotification] = useState(false);
   return (
-    <div className="flex items-center gap-4">
-      <img src={NotificationIcon} alt="Notification" className="w-8 h-8" />
+    <div className="relative flex items-center gap-4">
+      <img
+        src={NotificationIcon}
+        alt="Notification"
+        className="w-8 h-8 cursor-pointer"
+        onClick={() => setShowNotification(!showNotification)}
+      />
+      <Notifications user={user} display={showNotification} />
       <p
         className="text-4xl font-normal cursor-pointer"
         title={`Access Level : ${user.role}`}
@@ -30,6 +38,59 @@ const User = ({ user }) => {
       </p>
     </div>
   );
+};
+
+const Notifications = ({ user, display }) => {
+  const [notifications, setNotifications] = useState([]);
+  const accessToken = useRecoilValue(accessTokenAtom)
+
+  useEffect(() => {
+    const fetch = async () => {
+      if (user.username !== "") {
+        const response = await axios.get(
+          `http://localhost:3000/notifications/${user.username}`,{
+            headers:{
+              'Authorization': `Bearer ${accessToken}`,
+              'Content-Type': `application/json`,
+            }
+          }
+        );
+        const userNotifications = response.data.notifications ?? [];
+        setNotifications([...userNotifications]);
+      }
+    };
+
+    fetch();
+  }, []);
+
+  if (notifications.length === 0) {
+    return (
+      <div
+        className={`fixed mt-48 bg-white border-2 border-purple-400 px-4 py-2 ${
+          display ? "inline" : "hidden"
+        } z-20 rounded-md`}
+      >
+        <p className="text-2xl text-gray-400 border-b-2 mb-2">
+          No New Notifications
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={`fixed mt-48 bg-white border-2 border-purple-400 px-4 py-2 ${
+          display ? "inline" : "hidden"
+        } z-20 rounded-md`}
+      >
+        {notifications.map((notifications, index) => (
+          <div className=" border-b-2 mb-2 border-gray-200 flex flex-col items-end" key={index}>
+            <p className="text-2xl text-gray-400">{notifications.info}</p>
+            <span className="text-xs text-gray-500">{notifications.createdAt}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 };
 
 const Links = ({ user }) => {
@@ -90,9 +151,9 @@ const Links = ({ user }) => {
                 </li>
               </>
             )}
-            
+
             <li className="text-xl px-4 py-2 rounded-md text-gray-500  cursor-pointer">
-            <NavLink
+              <NavLink
                 to={"/signout"}
                 className={"text-2xl font-medium text-[#6A43C7] cursor-pointer"}
               >
