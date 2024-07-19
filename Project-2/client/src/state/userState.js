f2a8c3083209cafa6b85a691bc770474bce0ba01
Vivek <ojhavivek24@gmail.com>
2024-07-19 09:49:55 +0530

    added user state

---
+++ Project-2/client/src/state/userState.js
@@ -0,0 +1,52 @@
+import { atom } from 'recoil';
+
+export const userAtom = atom({
+    key: "userAtom",
+    default: {
+        username: "",
+        fullName: "",
+        role: "",
+        isAuthenticated: false
+    },
+    effects: [
+        ({ onSet, setSelf }) => {
+            const userInfo = JSON.parse(localStorage.getItem("userInfo"))
+
+            if (userInfo && userInfo.isAuthenticated) {
+                setSelf({
+                    username: userInfo.username,
+                    fullName: userInfo.fullName,
+                    role: userInfo.role,
+                    isAuthenticated: userInfo.isAuthenticated
+                })
+            }
+
+            onSet(
+                (newState) => {
+                    setSelf(newState)
+                    localStorage.setItem("userInfo", JSON.stringify(newState))
+                }
+            )
+        }
+    ]
+})
+
+export const accessTokenAtom = atom({
+    key: "accessTokenAtom",
+    default: "",
+    effects: [
+        ({ setSelf, onSet }) => {
+            const { accessToken } = JSON.parse(localStorage.getItem("token"))
+
+            if (accessToken) {
+                setSelf(accessToken)
+            }
+
+            onSet((newToken) => {
+                setSelf(newToken);
+                localStorage.setItem("accessToken", JSON.stringify({ accessToken: newToken }))
+            })
+
+        }
+    ]
+})
